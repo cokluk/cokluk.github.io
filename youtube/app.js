6ee90bce4d6a7de0f1da0201184360a58fc95dd9
C O K L U K <47196492+cokluk@users.noreply.github.com>
2021-06-11 06:54:37 +0300

    fullscreen

--- youtube/app.js
+++ youtube/app.js
@@ -154,7 +154,7 @@ var Element = `
 <br>
 <center><img style="width:30%;" id="yt" src="ytw.png" /></center>
 <a id="i_2" onclick="tamEkran()">Tam ekran</a>
-<iframe  id="ytframe" style="width:200px;border:none;margin-top: 100px;" src="https://www.youtube.com/embed/${x}?autoplay=1" allow='autoplay'></iframe>
+<iframe  id="ytframe" style="width:200px;border:none;margin-top: 10px;" src="https://www.youtube.com/embed/${x}?autoplay=1" allow='autoplay'></iframe>
  
 <a id="i_1" onclick="vazgec()">İzlemeyi bırak</a>
 
